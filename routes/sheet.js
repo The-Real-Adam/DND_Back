946cb91e14d94fafc1d5b87013b2cfd3c174b41b
Adam <Adam@Adams-MacBook-Pro-3.local>
2018-01-16 14:59:11 -0700

    trying a new log

--- routes/sheet.js
+++ routes/sheet.js
@@ -7,6 +7,7 @@ const SECRET = process.env.JWT_KEY
 const auth = (req, res, next) => {
   console.log('SECRET IN SHEETS IS: ', SECRET)
   console.log('cookies', req.cookies)
+  console.log('request is: >>>>>>>', req)
   jwt.verify(req.cookies.dnd, SECRET, (err, payload) => {
     if (err) {
       console.log('you hit your own error code dumbass', err);
